cc220c6af7e340dda607dde0c24083a64e9e0261
manuellopper <96139661+manuellopper@users.noreply.github.com>
2023-09-26 21:46:27 +0200

    Updated with the help of ChatGPT

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+```python
 import getpass
 from github import Github, GithubException, InputGitTreeElement
 import openai
@@ -5,34 +6,22 @@ from config import api_key_openai, api_key_github
 import random
 import string
 import re
-
-# Authentication is defined via github.Auth
 from github import Auth
-
 try:
-    # using an access token
     auth = Auth.Token(api_key_github)
-    # Connect to the repository
     g = Github(auth=auth)
     user = g.get_user()
-
-    # List all repositories and allow the user to choose one
     print("Your repositories:")
     repos = list(user.get_repos())
     for i, repo in enumerate(repos):
         print(f"{i+1}. {repo.name}")
     repo_choice = int(input("Choose the number of the repository you want to work on: ")) - 1
     repo = repos[repo_choice]
-
-    # Ask the user if they want to work on an existing file or create a new one
     file_choice = input("Do you want to work on an existing file (E) or create a new one (N)? ")
-
     if file_choice.lower() == 'n':
-        # Ask the user to enter the name of the file
         file_name = input("Enter the name of the new file: ")
         file_content = ""
     else:
-        # Show all project files and allow the user to choose one
         print("Files in this repository:")
         files = repo.get_contents("")
         for i, file in enumerate(files):
@@ -40,11 +29,7 @@ try:
         num_file_choice = int(input("Choose the number of the file you want to work on: ")) - 1
         file = files[num_file_choice]
         file_content = file.decoded_content.decode()
-
-    # Ask the user how the AI can help
     prompt = input("How can the AI help you write code? ")
-
-    # Invoke the ChatGPT API
     openai.api_key = api_key_openai
     response = openai.ChatCompletion.create(
       model="gpt-4",
@@ -55,26 +40,25 @@ try:
         ],
       temperature=0
     )
-
     new_code = response['choices'][0]['message']['content'].strip()
-
-    # Separate code and text
     code_lines = []
     text_lines = []
+    in_code_block = False
     for line in new_code.split('\n'):
-        if re.match(r'^\s*#', line) or re.match(r'^\s*$', line):
-            text_lines.append(line)
-        else:
+        if '```python' in line:
+            in_code_block = True
+            continue
+        elif '```' in line:
+            in_code_block = False
+            continue
+        if in_code_block:
             code_lines.append(line)
-
+        else:
+            text_lines.append(line)
     new_code = '\n'.join(code_lines)
     text_content = '\n'.join(text_lines)
-
-    # Print the text content
     print("Text content:")
     print(text_content)
-
-    # Create a new branch
     source_branch = repo.get_branch("main")
     branch_name = "new_branch"
     while True:
@@ -83,21 +67,13 @@ try:
             break
         except GithubException:
             branch_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
-
     print(f"New branch created: {branch_name}")
-
-    # Update the file in the new branch
     if file_choice.lower() == 'n':
         repo.create_file(file_name, "Created with the help of ChatGPT", new_code, branch=branch_name)
     else:
         repo.update_file(file.path, "Updated with the help of ChatGPT", new_code, file.sha, branch=branch_name)
-
-    # Create a pull request
     repo.create_pull(title="Pull Request from ChatGPT", body="Here are the changes made by ChatGPT", base="main", head=branch_name)
-
     print("Success! The operations were completed successfully.")
-
-    # Provide a summary of the tasks performed
     print("\nSummary of tasks performed:")
     print(f"1. Connected to the repository: {repo.name}")
     print(f"2. Created a new branch: {branch_name}")
@@ -106,10 +82,10 @@ try:
     else:
         print(f"3. Updated the existing file: {file.path}")
     print("4. Created a pull request")
-
 except GithubException as e:
     print(f"An error occurred with GitHub: {e}")
 except openai.OpenAIError as e:
     print(f"An error occurred with OpenAI: {e}")
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
+```
